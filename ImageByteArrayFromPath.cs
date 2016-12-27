ac1a07284f80ce10aaeb911fb8dc7f3e0cfe8ca5
Lucas Moura Veloso <lmouraveloso@gmail.com>
2016-12-27 09:26:22 -0200

    Dependency services for xamarin

---
+++ ImageByteArrayFromPath.cs
@@ -0,0 +1,21 @@
+//For Android
+public byte[] ByteArrayFromStream(string path)
+{
+	var bitmap = BitmapFactory.DecodeFile(path);
+	
+	byte[] bitmapData;
+	using(stream = new MemoryStream())
+	{
+		bitmap.Compress(Bitmap.CompressFormat.Png, 50, stream);
+		bitmapData = stream.ToArray();
+	}
+	
+	return bitmapData;
+}
+
+//For iOS
+public byte[] ByteArrayFromStream(string path)
+{
+	UIImage originalImage = UIImage.FromFile(path);
+	return originalImage.AsPNG().ToArray();
+}
---
+++ VideoPlayerFromPath.cs
@@ -0,0 +1,68 @@
+//For Android
+public void Play(string path)
+{
+	var bytes = File.ReadAllBytes(path);
+
+	string externalStorageState = global::Android.OS.Environment.ExternalStorageState;
+	string application = "";
+
+	string extension = System.IO.Path.GetExtension(path);
+
+	switch (extension.ToLower())
+	{
+		case ".mp4":
+			application = "video/mp4";
+			break;
+		case ".flv":
+			application = "video/x-flv";
+			break;
+		case ".m3u8":
+			application = "application/x-mpegURL";
+			break;
+		case ".ts":
+			application = "video/MP2T";
+			break;
+		case ".3gp":
+			application = "video/3gpp";
+			break;
+		case ".mov":
+			application = "video/quicktime";
+			break;
+		case ".avi":
+			application = "video/x-msvideo";
+			break;
+		case ".wmv":
+			application = "video/x-ms-wmv";
+			break;
+		default:
+			application = "*/*";
+			break;
+	}
+
+	Java.IO.File file = new Java.IO.File(path);
+	file.SetReadable(true);
+	Android.Net.Uri uri = Android.Net.Uri.FromFile(file);
+	Intent intent = new Intent(Intent.ActionView);
+	intent.SetDataAndType(uri, application);
+	intent.SetFlags(ActivityFlags.ClearWhenTaskReset | ActivityFlags.NewTask);
+
+	Xamarin.Forms.Forms.Context.StartActivity(intent);
+}
+
+//For iOS
+public void Play(string path)
+{
+	var _player = new AVPlayer(NSUrl.FromFilename(path));
+	var _playerController = new AVPlayerViewController();
+	_playerController.Player = _player;
+
+	var window = UIApplication.SharedApplication.KeyWindow;
+	var vc = window.RootViewController;
+	while (vc.PresentedViewController != null)
+	{
+		vc = vc.PresentedViewController;
+	}
+
+	vc.PresentViewController(_playerController, true, null);
+	_playerController.View.Frame = vc.View.Frame;
+}
